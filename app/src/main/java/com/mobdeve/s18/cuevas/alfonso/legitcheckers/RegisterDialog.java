fb4abc9d0e4f7f9b0fc6ca3d4811b4db09a08160
Scoopdiddy-whoop <80666004+Scoopdiddy-whoop@users.noreply.github.com>
2021-08-19 14:21:12 +0800

    Update RegisterDialog.java

--- app/src/main/java/com/mobdeve/s18/cuevas/alfonso/legitcheckers/RegisterDialog.java
+++ app/src/main/java/com/mobdeve/s18/cuevas/alfonso/legitcheckers/RegisterDialog.java
@@ -5,7 +5,6 @@ import android.app.Dialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatDialogFragment;
