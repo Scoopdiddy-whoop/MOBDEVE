37aeecf8827e189d8aa77eb847dec459f4ccfe4c
Scoopdiddy-whoop <80666004+Scoopdiddy-whoop@users.noreply.github.com>
2021-08-25 23:11:45 +0800

    updatedd database and user model
    edited database helpers
    added more database helpers for wins and lossses
    added wins and losses to user model

--- app/src/main/java/com/mobdeve/s18/cuevas/alfonso/legitcheckers/model/Database.java
+++ app/src/main/java/com/mobdeve/s18/cuevas/alfonso/legitcheckers/model/Database.java
@@ -1,33 +1,26 @@
 package com.mobdeve.s18.cuevas.alfonso.legitcheckers.model;
 
-import android.annotation.SuppressLint;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
 
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class Database {
-    private FirebaseUser firebaseUser;
     private FirebaseFirestore db;
 
 
     public Database() {
         db = FirebaseFirestore.getInstance();
     }
-    public boolean addUser(User user){
-        final boolean added[] = new boolean[1];
+    public void addUser(User user){
         db.collection("users").document(user.getId()).set(new HashMap()).addOnSuccessListener(new OnSuccessListener<Void>() {
             @Override
             public void onSuccess(Void unused) {
@@ -35,18 +28,16 @@ public class Database {
                 updateFriendList(user, user.getFriendlist());
                 updateMatches(user, user.getMatches());
                 updateUsername(user, user.getUsername());
+                updateWins(user, user.getWins());
+                updateLosses(user, user.getLosses());
 
-                added[0]=true;
             }
         }).addOnFailureListener(new OnFailureListener() {
             @Override
             public void onFailure(@NonNull Exception e) {
                 Log.i("DATABASE", "Account not added");
-                added[0]=false;
             }
         });
-
-        return added[0];
     }
 
     public void addFriend(User user, String friendID){
@@ -91,7 +82,7 @@ public class Database {
         return valid[0];
 
     }
-    public Map getFriendList(User user, FirebaseMapCallback firebaseMapCallback){
+    public void getFriendList(User user, FirebaseMapCallback firebaseMapCallback){
         DocumentReference docRef = db.collection("users").document(user.getId());
         final Map<String, Object>[] friendlist = new Map[1];
 
@@ -99,7 +90,7 @@ public class Database {
             if(task.isSuccessful()) {
                 DocumentSnapshot document = task.getResult();
                 if (document.exists()) {
-                    friendlist[0] = (Map) document.getData().get("friendlist");
+                    friendlist[0] = (Map)document.getData().get("friendlist");
                     Log.i("DATABASE", "Friendlist data: " + friendlist[0]);
                     firebaseMapCallback.onCallBack(friendlist[0]);
 
@@ -107,33 +98,30 @@ public class Database {
                     Log.i("DATABASE", "No friendlist data");
                 }
             }
-            else{
-                Log.i("DATABASE", "it gone done did it");
-            }
         });
-        return friendlist[0];
     }
-    public boolean addMatch(User user, String matchID){
-        Map<String, Object> matches = getMatches(user);
-        matches.put("matchID",  matchID);
-        final boolean[] valid = new boolean[1];
-
-        db.collection("users").document(user.getId()).update("matches", matches).addOnSuccessListener(new OnSuccessListener<Void>() {
+    public void addMatch(User user, String matchID){
+        getMatches(user, new FirebaseMapCallback() {
             @Override
-            public void onSuccess(Void aVoid) {
-                valid[0] = true;
-                Log.i("DATABASE", "Match added to match list");
-            }
+            public void onCallBack(Map<String, Object> matches) {
+                matches.put("matchID",  matchID);
+                final boolean[] valid = new boolean[1];
+                db.collection("users").document(user.getId()).update("matches", matches).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        valid[0] = true;
+                        Log.i("DATABASE", "Match added to match list");
+                    }
 
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                valid[0] = false;
-                Log.i("DATABASE", "Error adding match", e);
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        valid[0] = false;
+                        Log.i("DATABASE", "Error adding match", e);
+                    }
+                });
             }
         });
-
-        return valid[0];
     }
     public boolean updateMatches(User user, Map matches) {
         Map<String, Object> updatedList = matches;
@@ -158,7 +146,7 @@ public class Database {
         return valid[0];
 
     }
-    public Map getMatches(User user){
+    public Map getMatches(User user, FirebaseMapCallback firebaseMapCallback){
         DocumentReference docRef = db.collection("users").document(user.getId());
         final Map<String, Object>[] matches = new Map[1];
 
@@ -167,15 +155,16 @@ public class Database {
                 DocumentSnapshot document = task.getResult();
                 if (document.exists()) {
                     matches[0] = (Map) document.getData().get("matches");
-                    Log.d("DATABASE", "Matches data: " + matches[0]);
+                    Log.i("DATABASE", "Matches data: " + matches[0]);
+                    firebaseMapCallback.onCallBack(matches[0]);
                 } else {
-                    Log.d("DATABASE", "No Matches data");
+                    Log.i("DATABASE", "No Matches data");
                 }
             }
         });
         return matches[0];
     }
-    public String getUsername(User user) {
+    public String getUsername(User user, FirebaseStringCallback firebaseStringCallback) {
         DocumentReference docRef = db.collection("users").document(user.getId());
         final String[] username = new String[1];
 
@@ -185,6 +174,7 @@ public class Database {
                 if (document.exists()) {
                     username[0] = (String) document.getData().get("username");
                     Log.d("DATABASE", "Username data: " + username[0]);
+                    firebaseStringCallback.onCallBack(username[0]);
                 } else {
                     Log.d("DATABASE", "No username data");
                 }
@@ -214,8 +204,123 @@ public class Database {
 
         return valid[0];
     }
+    public void addWin(User user) {
+        getWins(user, new FirebaseIntCallback() {
+            @Override
+            public void onCallBack(int wins) {
+                wins+=1;
+                db.collection("users").document(user.getId()).update("wins", wins).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.i("DATABASE", "Wins has been increased by 1");
+                    }
+
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.i("DATABASE", "Error adding to wins", e);
+                    }
+                });
+            }
+        });
+    }
+    public void getWins(User user, FirebaseIntCallback firebaseIntCallback) {
+        DocumentReference docRef = db.collection("users").document(user.getId());
+        final int[] wins = new int[1];
+
+        docRef.get().addOnCompleteListener(task -> {
+            if(task.isSuccessful()) {
+                DocumentSnapshot document = task.getResult();
+                if (document.exists()) {
+                    wins[0] = (int) document.getData().get("wins");
+                    Log.i("DATABASE", "Wins data: " + wins[0]);
+                    firebaseIntCallback.onCallBack(wins[0]);
+
+                } else {
+                    Log.i("DATABASE", "No wins data");
+                }
+            }
+        });
+    }
+    public void updateWins(User user, int wins) {
+        int updatedWins = wins;
+
+        db.collection("users").document(user.getId()).update("wins", updatedWins).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Log.i("DATABASE", "Wins has been updated ");
+            }
+
+        })
+        .addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.i("DATABASE", "Error in updating wins", e);
+            }
+        });
+    }
+    public void addLoss(User user){
+        getLosses(user, new FirebaseIntCallback() {
+            @Override
+            public void onCallBack(int losses) {
+                losses+=1;
+                db.collection("users").document(user.getId()).update("losses", losses).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.i("DATABASE", "Losses has been increased by 1");
+                    }
+
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.i("DATABASE", "Error adding to losses", e);
+                    }
+                });
+            }
+        });
+    }
+    public void getLosses(User user, FirebaseIntCallback firebaseIntCallback) {
+        DocumentReference docRef = db.collection("users").document(user.getId());
+        final int[] losses = new int[1];
+
+        docRef.get().addOnCompleteListener(task -> {
+            if(task.isSuccessful()) {
+                DocumentSnapshot document = task.getResult();
+                if (document.exists()) {
+                    losses[0] = (int) document.getData().get("losses");
+                    Log.i("DATABASE", "Losses data: " + losses[0]);
+                    firebaseIntCallback.onCallBack(losses[0]);
+                } else {
+                    Log.i("DATABASE", "No Losses data");
+                }
+            }
+        });
+    }
+    public void updateLosses(User user, int losses) {
+        int updatedLosses = losses;
+
+        db.collection("users").document(user.getId()).update("losses", updatedLosses).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Log.i("DATABASE", "Losses has been updated ");
+            }
+
+        })
+        .addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.i("DATABASE", "Error in updating Losses", e);
+            }
+        });
+    }
     public interface FirebaseMapCallback{
         void onCallBack(Map<String, Object> map);
+    }
+    public interface FirebaseStringCallback{
+        void onCallBack(String string);
+    }
+    public interface FirebaseIntCallback{
+        void onCallBack(int number);
     }
 }
 
--- app/src/main/java/com/mobdeve/s18/cuevas/alfonso/legitcheckers/model/User.java
+++ app/src/main/java/com/mobdeve/s18/cuevas/alfonso/legitcheckers/model/User.java
@@ -8,19 +8,25 @@ public class User {
     private Map<String, Object> friendlist;
     private Map<String, Object> matches;
     private String username;
+    private int wins;
+    private int losses;
 
     public User(String id, Map friendlist,
-                Map matches, String username){
+                Map matches, String username, int wins, int losses){
         this.id = id;
         this.friendlist = friendlist;
         this.matches = matches;
         this.username = username;
+        this.wins = wins;
+        this.losses = losses;
     }
     public User(String id, String username) {
         this.id = id;
         this.username = username;
         this.friendlist = new HashMap<>();
         this.matches = new HashMap<>();
+        this.wins = 0;
+        this.losses = 0;
     }
     public String getId() {
         return id;
@@ -53,4 +59,22 @@ public class User {
     public void setUsername(String username) {
         this.username = username;
     }
+
+    public int getWins() {
+        return wins;
+    }
+
+    public void setWins(int wins) {
+        this.wins = wins;
+    }
+
+    public int getLosses() {
+        return losses;
+    }
+
+    public void setLoss(int loss) {
+        this.losses = loss;
+    }
+
+
 }
